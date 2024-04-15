d2a4b9ef90b1ad91f6a0118d601876a0c53910f4
Ian Abenes <ian.abenes@gmail.com>
2024-04-15 00:03:59 -0500

    create exploratory script
    created functions for cleaning, splitting cabin number into separate variables
    
    created pure pilot dataset with no missing values

---
+++ pilot study/explore_data.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv("pilot.csv")
+
+def clean_data(data): 
+    # Removes unneccessary columns
+    data.drop(axis=1, labels=["Unnamed: 0", "PassengerId"])
+    return data
+
+def split_cabin_label(cabin_string): 
+    # Splits "Cabin" variable into three variables
+    # get cabin_string using df.loc[index, "Cabin"]
+    cabin_list = cabin_string.split("/")
+    cabin_list[1] = int(cabin_list[1])
+    return cabin_list
+
+df_clean = clean_data(df)
+df_pure = df_clean.dropna(axis=0, how="any")
+df_pure.to_csv("pilot_pure.csv")
