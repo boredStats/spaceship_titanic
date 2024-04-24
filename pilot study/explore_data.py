ca0b0cbfcc75803e8b274ba9fc57ced87e0d4514
Ian Abenes <ian.abenes@gmail.com>
2024-04-23 23:03:35 -0500

    overhaul, keeping NaN

--- pilot study/explore_data.py
+++ pilot study/explore_data.py
@@ -3,36 +3,33 @@ import pandas as pd
 
 df = pd.read_csv("pilot.csv")
 
-def clean_data(data): 
-    # Removes unneccessary columns
-    data.drop(axis=1, labels=["Unnamed: 0", "PassengerId"])
-    return data
-
-def split_cabin_label(cabin_string): 
-    # Splits "Cabin" variable into three variables
-    # get cabin_string using df.loc[index, "Cabin"]
-    cabin_list = cabin_string.split("/")
-    # cabin_list[1] = int(cabin_list[1]) # keeping cabin number as a nominal variable
-    return cabin_list
-
-df_clean = clean_data(df)
-df_pure = df_clean.dropna(axis=0, how="any")
-df_pure.to_csv("pilot_pure.csv")
+df.drop(columns=["Unnamed: 0", "PassengerId"], inplace=True)
 
 vars_of_interest = ['HomePlanet', 'CryoSleep', 'Cabin', 'Destination', 'VIP','Transported'] 
-df_test = df_pure[vars_of_interest]
+df_test = df[vars_of_interest]
 
-x, y, z = [], [], []
+def split_cabin_label(cabin_string): 
+    # Splits "Cabin" variable
+    # get cabin_string using df['Cabin'].iloc[index]
+    if type(cabin_string) == float:
+        return float('NaN'), float('Nan') # catching missing data
+    else:
+        cabin_list = cabin_string.split("/")
+        cabin_deck = cabin_list[0]
+        cabin_side = cabin_list[2]
+        return cabin_deck, cabin_side
+    
+cd_list, cs_list, z = [], [], []
 for i in range(len(df_test.index)):
     g = df_test['Cabin'].iloc[i]
-    val = split_cabin_label(g)
-    x.append(val[0])
-    y.append(val[1])
-    z.append(val[2])
+    x, y = split_cabin_label(g)
+    cd_list.append(x)
+    cs_list.append(y)
 
 df_test.drop(columns='Cabin', inplace=True)
-df_test.insert(4,'CabinDeck', x)
-# df_test.insert(6,'CabinNum', y) # removing cabin number variable
-df_test.insert(5, 'CabinSide', z)
+df_test.insert(4,'CabinDeck', cd_list)
+df_test.insert(5, 'CabinSide', cs_list)
 
 df_test.to_csv("pilot_test.csv")
+
+print(df_test.describe())
